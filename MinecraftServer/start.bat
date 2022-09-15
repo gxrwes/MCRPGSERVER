2d198e038ee36cd37be800e2b6d8a5d8181c6498
gxrwes <wesley.st96@yahoo.de>
2022-09-15 20:12:15 +0200

    working alpha server

---
+++ MinecraftServer/start.bat
@@ -0,0 +1,12 @@
+:: Start without GUI
+echo STARTING MC-Server without GUI
+@echo off
+java -Xms2G -Xmx2G -XX:+UseG1GC -jar spigot-1.19.2.jar nogui
+pause
+
+
+:: Start with GUI
+::echo STARTING MC-Server with GUI
+::@echo off
+::java -Xms2G -Xmx2G -XX:+UseG1GC -jar spigot-1.18.jar
+::pause
